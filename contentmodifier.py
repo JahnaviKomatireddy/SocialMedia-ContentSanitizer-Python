4d489b35e5c36dc4d95d9236c2448fcb68de326f
JahnaviKomatireddy <jahnavikomatireddy@gmail.com>
2026-04-29 11:25:43 +0530

    Initial commit - Social Media Content Sanitizer

--- contentmodifier.py
+++ contentmodifier.py
@@ -1,44 +1,51 @@
+# Social Media Content Sanitizer
+import re
 
 n = int(input("Enter number of posts: "))
 posts = []
+
 for i in range(n):
     user = input("Enter username: ")
     post = input("Enter post: ")
     posts.append((user, post))
+bannedwords = ["bad", "toxic", "hate", "bully", "spam", "worst", "useless", "dumb", "bullied"]
 
-bannedwords = ["bad", "toxic", "hate","bully","spam","worst","useless","dumb"]
 totalPosts = len(posts)
 postscleaned = 0
 cleanposts = 0
 user_flags = {}
-links_file = open("../links_found.txt", "w")
+links_file = open("links_found.txt", "w")
+
+pattern = r'\b(' + '|'.join(bannedwords) + r')\b'
+def mask(match):
+    return "*" * len(match.group())
 
 print("\nPosts:")
+
 for user, post in posts:
     original = post
-    flagged = False
     if user not in user_flags:
         user_flags[user] = 0
-    for word in bannedwords:
-        if word in post.lower():
-            post = post.replace(word, "***")
-            flagged = True
-    words = original.split()
-    for w in words:
-        if w.startswith("http"):
-            links_file.write(w + "\n")
-    if flagged:
+
+    new_post = re.sub(pattern, mask, post, flags=re.IGNORECASE)
+    if new_post != post:
         postscleaned += 1
         user_flags[user] += 1
     else:
         cleanposts += 1
-    print(f"{user}: {post}")
+
+    words = original.split()
+    for w in words:
+        if w.startswith("http"):
+            links_file.write(w + "\n")
+    print(f"{user}: {new_post}")
 
 links_file.close()
-print("Final Report:")
+
+print("\nFinal Report:")
 print(f"Total Posts Screened: {totalPosts}")
 print(f"Cleaned Posts: {postscleaned}")
 print(f"Clean Posts: {cleanposts}")
 
-print("Number of times a user post have been modified or flagged")
+print("\nUser Flag Summary:")
 print(user_flags)
